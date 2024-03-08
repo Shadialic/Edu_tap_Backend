45f498209663b757b209621fe3f4f67fdbc78f3b
shadil <shadilsa786@gmai.com>
2024-03-08 11:25:22 +0530

    url correct it

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://www.malefashion.fun",
     credentials: true,
     methods: "*",
   })
--- socket/socket.js
+++ socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://www.malefashion.fun",
     methods: ["GET", "POST", "PUT"],
     credentials: true,
   },
